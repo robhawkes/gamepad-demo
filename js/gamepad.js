b1f7ed0b2b701a18a4c1f81b109dac37b1393e2f
Rob Hawkes <rob@rawkes.com>
2012-02-12 21:17:41 +0000

    - Show gamepad disconnected message if required - Automatically show the correct gamepad on connection - Updated known issues

--- js/gamepad.js
+++ js/gamepad.js
@@ -23,7 +23,7 @@ var XBOX_BUTTONS = {
 	RIGHT_STICK: 7,
 	START: 8,
 	BACK: 9,
-	XBOX: 10,
+	HOME: 10,
 	DPAD_UP: 11,
 	DPAD_DOWN: 12,
 	DPAD_LEFT: 13,
@@ -48,7 +48,7 @@ var PS3_BUTTONS = {
 	RIGHT_STICK: 2,
 	START: 3,
 	SELECT: 0,
-	PS: 16,
+	HOME: 16,
 	DPAD_UP: 4,
 	DPAD_DOWN: 6,
 	DPAD_LEFT: 7,
@@ -56,65 +56,49 @@ var PS3_BUTTONS = {
 };
 
 var controller,
-	controllerType = 0, // Default to xBox controller
+	controllerType,
 	// xBox/default controls
-	leftStick = document.getElementById("leftStick"),
-	rightStick = document.getElementById("rightStick"),
-	backButton = document.getElementById("backButton"),
-	xboxButton = document.getElementById("xboxButton"),
-	startButton = document.getElementById("startButton"),
-	dpadUp = document.getElementById("dpadUp"),
-	dpadDown = document.getElementById("dpadDown"),
-	dpadLeft = document.getElementById("dpadLeft"),
-	dpadRight = document.getElementById("dpadRight"),
-	aButton = document.getElementById("aButton"),
-	bButton = document.getElementById("bButton"),
-	xButton = document.getElementById("xButton"),
-	yButton = document.getElementById("yButton"),
-	lbButton = document.getElementById("lbButton"),
-	rbButton = document.getElementById("rbButton"),
+	xBoxLeftStick = document.getElementById("xBoxLeftStick"),
+	xBoxRightStick = document.getElementById("xBoxRightStick"),
+	xBoxBackButton = document.getElementById("xBoxBackButton"),
+	xBoxHomeButton = document.getElementById("xBoxHomeButton"),
+	xBoxStartButton = document.getElementById("xBoxStartButton"),
+	xBoxDpadUp = document.getElementById("xBoxDpadUp"),
+	xBoxDpadDown = document.getElementById("xBoxDpadDown"),
+	xBoxDpadLeft = document.getElementById("xBoxDpadLeft"),
+	xBoxDpadRight = document.getElementById("xBoxDpadRight"),
+	xBoxAButton = document.getElementById("xBoxAButton"),
+	xBoxBButton = document.getElementById("xBoxBButton"),
+	xBoxXButton = document.getElementById("xBoxXButton"),
+	xBoxYButton = document.getElementById("xBoxYButton"),
+	xBoxLbButton = document.getElementById("xBoxLbButton"),
+	xBoxRbButton = document.getElementById("xBoxRbButton"),
 
 	// PS3-specific controls
-	selectButton = document.getElementById("selectButton"),
-	psButton = document.getElementById("psButton"),
-	crossButton = document.getElementById("crossButton"),
-	circleButton = document.getElementById("circleButton"),
-	squareButton = document.getElementById("squareButton"),
-	triangleButton = document.getElementById("triangleButton"),
-	lb1Button = document.getElementById("lb1Button"),
-	rb1Button = document.getElementById("rb1Button"),
+	PS3LeftStick = document.getElementById("PS3LeftStick"),
+	PS3RightStick = document.getElementById("PS3RightStick"),
+	PS3SelectButton = document.getElementById("PS3SelectButton"),
+	PS3HomeButton = document.getElementById("PS3HomeButton"),
+	PS3StartButton = document.getElementById("PS3StartButton"),
+	PS3DpadUp = document.getElementById("PS3DpadUp"),
+	PS3DpadDown = document.getElementById("PS3DpadDown"),
+	PS3DpadLeft = document.getElementById("PS3DpadLeft"),
+	PS3DpadRight = document.getElementById("PS3DpadRight"),
+	PS3CrossButton = document.getElementById("PS3CrossButton"),
+	PS3CircleButton = document.getElementById("PS3CircleButton"),
+	PS3SquareButton = document.getElementById("PS3SquareButton"),
+	PS3TriangleButton = document.getElementById("PS3TriangleButton"),
+	PS3Lb1Button = document.getElementById("PS3Lb1Button"),
+	PS3Rb1Button = document.getElementById("PS3Rb1Button"),
 
 	// Controller change stuff
+	controllerDisconnected = document.getElementById("controllerDisconnected"),
 	controllerXbox = document.getElementById("xboxController"),
-	controllerPS3 = document.getElementById("ps3Controller"),
-	controllerChangeXbox = document.getElementById("select-xbox"),
-	controllerChangePS3 = document.getElementById("select-ps3");
+	controllerPS3 = document.getElementById("ps3Controller");
 
 // Listen for gamepad connection and disconnection
 window.addEventListener("MozGamepadConnected", onGamepadConnected);
 window.addEventListener("MozGamepadDisconnected", onGamepadDisconnected);
-
-// Listen for controller change events
-controllerChangeXbox.addEventListener("click", onSelectXbox);
-controllerChangePS3.addEventListener("click", onSelectPS3);
-
-// Run when xBox controller is selected
-function onSelectXbox(e) {
-	e.preventDefault();
-	controllerType = CONTROLLER_TYPES.XBOX;
-
-	controllerPS3.style.display = "none";
-	controllerXbox.style.display = "block";
-}
-
-// Run when PS3 controller is selected
-function onSelectPS3(e) {
-	e.preventDefault();
-	controllerType = CONTROLLER_TYPES.PS3;
-
-	controllerXbox.style.display = "none";
-	controllerPS3.style.display = "block";
-}
 
 // Run When a gamepad is connected
 function onGamepadConnected(e) {
@@ -134,6 +118,18 @@ function onGamepadConnected(e) {
 	// 	console.log(controller.axes[i]);
 	// };
 
+	// Work out and set controler type
+	switch (controller.id) {
+		case "54c-268-PLAYSTATION(R)3 Controller":
+			controllerType = CONTROLLER_TYPES.PS3;
+			controllerXbox.style.display = "none";
+			controllerPS3.style.display = "block";
+			break;
+	}
+
+	// Hide disconnection message and show relevant controller
+	controllerDisconnected.style.display = "none";
+
 	// Start controller checks
 	updateController();
 }
@@ -141,6 +137,13 @@ function onGamepadConnected(e) {
 // Run When a gamepad is disconnected
 function onGamepadDisconnected(e) {
 	console.log("Gamepad disconnected", controller.id);
+
+	// Hide controller visualisations
+	controllerXbox.style.display = "none";
+	controllerPS3.style.display = "none";
+
+	// Show disconnected message
+	controllerDisconnected.style.display = "block";
 }
 
 // Generic controller update check
@@ -186,49 +189,49 @@ function updateXboxController() {
 
 		switch (i) {
 			case XBOX_BUTTONS.A:
-				aButton.classList.add("pressed");
+				xBoxAButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.B:
-				bButton.classList.add("pressed");
+				xBoxBButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.X:
-				xButton.classList.add("pressed");
+				xBoxXButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.Y:
-				yButton.classList.add("pressed");
+				xBoxYButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.LB:
-				lbButton.classList.add("pressed");
+				xBoxLbButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.RB:
-				rbButton.classList.add("pressed");
+				xBoxRbButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.LEFT_STICK:
-				leftStick.classList.add("pressed");
+				xBoxLeftStick.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.RIGHT_STICK:
-				rightStick.classList.add("pressed");
+				xBoxRightStick.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.START:
-				startButton.classList.add("pressed");
+				xBoxStartButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.BACK:
-				backButton.classList.add("pressed");
+				xBoxBackButton.classList.add("pressed");
 				break;
-			case XBOX_BUTTONS.XBOX:
-				xboxButton.classList.add("pressed");
+			case XBOX_BUTTONS.HOME:
+				xBoxHomeButton.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.DPAD_UP:
-				dpadUp.classList.add("pressed");
+				xBoxDpadUp.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.DPAD_DOWN:
-				dpadDown.classList.add("pressed");
+				xBoxDpadDown.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.DPAD_LEFT:
-				dpadLeft.classList.add("pressed");
+				xBoxDpadLeft.classList.add("pressed");
 				break;
 			case XBOX_BUTTONS.DPAD_RIGHT:
-				dpadRight.classList.add("pressed");
+				xBoxDpadRight.classList.add("pressed");
 				break;
 		}
 	}
@@ -237,16 +240,16 @@ function updateXboxController() {
 	for (i = 0; i < controller.axes.length; i++) {
 		switch (i) {
 			case XBOX_AXES.LEFT_STICK_X:
-				leftStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*20).toString()+"px";
+				xBoxLeftStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*20).toString()+"px";
 				break;
 			case XBOX_AXES.LEFT_STICK_Y:
-				leftStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*20).toString()+"px";
+				xBoxLeftStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*20).toString()+"px";
 				break;
 			case XBOX_AXES.RIGHT_STICK_X:
-				rightStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*20).toString()+"px";
+				xBoxRightStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*20).toString()+"px";
 				break;
 			case XBOX_AXES.RIGHT_STICK_Y:
-				rightStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*20).toString()+"px";
+				xBoxRightStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*20).toString()+"px";
 				break;
 			case XBOX_AXES.LEFT_TRIGGER:
 				break;
@@ -283,49 +286,49 @@ function updatePS3Controller() {
 
 		switch (i) {
 			case PS3_BUTTONS.CROSS:
-				crossButton.classList.add("pressed");
+				PS3CrossButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.CIRCLE:
-				circleButton.classList.add("pressed");
+				PS3CircleButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.SQUARE:
-				squareButton.classList.add("pressed");
+				PS3SquareButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.TRIANGLE:
-				triangleButton.classList.add("pressed");
+				PS3TriangleButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.LB1:
-				lb1Button.classList.add("pressed");
+				PS3Lb1Button.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.RB1:
-				rb1Button.classList.add("pressed");
+				PS3Rb1Button.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.LEFT_STICK:
-				leftStick.classList.add("pressed");
+				PS3LeftStick.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.RIGHT_STICK:
-				rightStick.classList.add("pressed");
+				PS3RightStick.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.START:
-				startButton.classList.add("pressed");
+				PS3StartButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.SELECT:
-				selectButton.classList.add("pressed");
+				PS3SelectButton.classList.add("pressed");
 				break;
-			case PS3_BUTTONS.PS:
-				psButton.classList.add("pressed");
+			case PS3_BUTTONS.HOME:
+				PS3HomeButton.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.DPAD_UP:
-				dpadUp.classList.add("pressed");
+				PS3DpadUp.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.DPAD_DOWN:
-				dpadDown.classList.add("pressed");
+				PS3DpadDown.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.DPAD_LEFT:
-				dpadLeft.classList.add("pressed");
+				PS3DpadLeft.classList.add("pressed");
 				break;
 			case PS3_BUTTONS.DPAD_RIGHT:
-				dpadRight.classList.add("pressed");
+				PS3DpadRight.classList.add("pressed");
 				break;
 		}
 	}
@@ -334,16 +337,16 @@ function updatePS3Controller() {
 	for (i = 0; i < controller.axes.length; i++) {
 		switch (i) {
 			case PS3_AXES.LEFT_STICK_X:
-				leftStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*25).toString()+"px";
+				PS3LeftStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*25).toString()+"px";
 				break;
 			case PS3_AXES.LEFT_STICK_Y:
-				leftStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*25).toString()+"px";
+				PS3LeftStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*25).toString()+"px";
 				break;
 			case PS3_AXES.RIGHT_STICK_X:
-				rightStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*25).toString()+"px";
+				PS3RightStick.getElementsByTagName("span")[0].style.left = (controller.axes[i]*25).toString()+"px";
 				break;
 			case PS3_AXES.RIGHT_STICK_Y:
-				rightStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*25).toString()+"px";
+				PS3RightStick.getElementsByTagName("span")[0].style.top = (controller.axes[i]*25).toString()+"px";
 				break;
 		}
 	}
